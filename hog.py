6673c82b15d5e3946482f452879eb2cfa5fe0cda
MicolCozzarizza <104081509+MicolCozzarizza@users.noreply.github.com>
2023-07-14 10:34:13 +0200

    Update hog.py

--- hog.py
+++ hog.py
@@ -10,27 +10,21 @@ from sklearn import svm
 import numpy as np
 
 
-#dataset_path = r'C:\Users\mcozzarizza\Desktop\pear_img'
-dataset_path = r'C:\Users\mcozzarizza\Desktop\output_imgs'
+dataset_path = r'C:\Users\mcozzarizza\Desktop\pear_img'
+#dataset_path = r'C:\Users\mcozzarizza\Desktop\output_imgs'
 img_path = glob(join(dataset_path, '*'))
-
 
 #HOG
 def calculate_hog(index):
-    img = cv.imread(img_path[index])#   img = cv.imread(img_path[index], cv.IMREAD_GRAYSCALE)
+    img = cv.imread(img_path[index])
+    #img = cv.imread(img_path[index], cv.IMREAD_GRAYSCALE)
 
     #img = cv.resize(img, (128*4, 64*4))
     img = cv.resize(img, (64*2, 128*2))
     cv.imshow('image', img)
-    #img = canny_edges(index)
-    fd, hog_img = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), block_norm='L2-Hys', transform_sqrt=True, visualize=True, channel_axis = -1)#fd is feature matrix,
-    #fig, ax = plt.subplots(1, 1, figsize=(8, 4), sharex=True, sharey=True)
-    #ax.imshow(hog_img, cmap=plt.cm.gray)
-    #plt.show()
-    #print(fd)
+    fd, hog_img = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), block_norm='L2-Hys', transform_sqrt=True, visualize=True, channel_axis = -1)#fd is feature matrix
     cv.imshow('Hog image', hog_img)
     cv.waitKey(0)
-    #print(fd)
     return fd, hog_img
 
 calculate_hog(27)
